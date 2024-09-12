351b4e999af9b4ec04ea347c73e7881339514207
NixtonM <nixton.meyer@gmail.com>
2024-09-12 13:41:30 +0200

    State after separating out ``pc_tiling``

---
+++ include/pc_tiling.h
@@ -0,0 +1,21 @@
+#ifndef PC_TILING_H
+#define PC_TILING_H
+
+#include <string>
+
+bool tile_point_clouds(std::string firstPointCloud,
+					   std::string secondPointCloud,
+					   int maxPointsPerTile,
+					   int minPointsPerTile,
+					   bool voxelGridFlag,
+					   float voxelGridFilterSize,
+					   float overlapTiles,
+					   int projectionDirection,
+					   bool verbose);
+
+
+bool resave_point_cloud(std::string firstPointCloud,
+					   std::string secondPointCloud,
+					   bool verbose);
+
+#endif // PC_TILING_H
---
+++ swig/pc_tiling.i
@@ -0,0 +1,12 @@
+ /* pc_tiling.i */
+ %module pc_tiling
+
+ %{
+	#define SWIG_FILE_WITH_INIT
+ 	#include "pc_tiling.h"
+ %}
+
+ %include std_string.i
+ %include std_vector.i
+
+ %include "pc_tiling.h"
